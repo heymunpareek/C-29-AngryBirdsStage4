1d822da76eb7171b0bcddd5d7a4187880611c0be
heymunpareek <amita.t.pareek@gmail.com>
2021-03-14 14:36:12 +0530

    Changed sketch and slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,11 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+
+        this.s1 = loadImage("sprites/sling1.png");
+        this.s2 = loadImage("sprites/sling2.png");
+        this.s3 = loadImage("sprites/sling3.png");
+        
         World.add(world, this.sling);
     }
 
@@ -16,11 +21,28 @@ class SlingShot{
     }
 
     display(){
+        image(this.s1, 200, 20);
+        image(this.s2, 170, 20);
+
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(79, 46, 20)
+            if(pointA.x < 220) {
+                strokeWeight(4);
+                line(pointA.x -25, pointA.y, pointB.x- 10, pointB.y);
+                line(pointA.x - 25, pointA.y, pointB.x + 30, pointB.y - 3)
+                image(this.s3, pointA.x - 30, pointA.y- 10, 15, 30)
+            }
+
+            else {
+                strokeWeight(4);
+                line(pointA.x +25, pointA.y, pointB.x- 10, pointB.y);
+                line(pointA.x +25, pointA.y, pointB.x + 30, pointB.y - 3)
+                image(this.s3, pointA.x + 25, pointA.y- 10, 15, 30)
+            }
+            
         }
     }
     
